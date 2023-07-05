00b8f413467ff3418b8471a3fc1ba1ca922e1e77
Abdulakeem Babatunde Bolarinwa <bolarinwa_akeem@yahoo.com>
2023-07-05 17:25:16 +0100

    first layer of navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 // import { useState } from 'react'
 import { Box, Text } from '@chakra-ui/react'
+import ReuseableButton from './components/ReuseableButton';
 
 function App() {
   
   return (
-    <Box bg='blue.100' w='100%' p={4} color='red'>
-      <Text>
-      Enjoy 10% cash back when you shop with us... Enjoy free delivery when you shop over 30k woth of drugs
+    <Box bg='blue.100' w='100%' p={2} color='red' display='flex' alignItems='center' justifyContent='space-evenly'>
+      <Text fontWeight='bold'>
+      Enjoy 10% cash back when you shop with us...... Enjoy free delivery when you shop over 30k woth of drugs
       </Text>
+      <ReuseableButton>
+        shop now
+      </ReuseableButton>
     </Box>
   )
 }
