d22bb8803b323ef6cc759abdc8836ed961546813
Abdulakeem Babatunde Bolarinwa <bolarinwa_akeem@yahoo.com>
2023-07-06 03:51:13 +0100

    done with navBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 // import { useState } from 'react'
 import { Box, Text } from '@chakra-ui/react'
 import ReuseableButton from './components/ReuseableButton';
+import WithSubnavigation from './components/NavBar';
 
 function App() {
   
   return (
-    <Box bg='blue.100' w='100%' p={2} color='red' display='flex' alignItems='center' justifyContent='space-evenly'>
-      <Text fontWeight='bold'>
+    <>
+    <Box bg='blue.100'  p={2} color='red' display='flex' alignItems='center' justifyContent='space-evenly' flexWrap={['wrap', 'nowrap']}>
+      <Text  textAlign={['center', 'left']} fontSize={['sm', 'md', 'lg']}>
       Enjoy 10% cash back when you shop with us...... Enjoy free delivery when you shop over 30k woth of drugs
       </Text>
       <ReuseableButton>
         shop now
       </ReuseableButton>
     </Box>
+    <WithSubnavigation />
+    </>
   )
 }
 
